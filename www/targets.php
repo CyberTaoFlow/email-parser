24c83ea55dabf453b5ce892159220c5fe00261ba
Jonathan Pulsifer <jonathan.pulsifer@gmail.com>
2015-04-09 14:02:13 -0400

    Pre styling

--- www/targets.php
+++ www/targets.php
@@ -35,6 +35,14 @@
 		}
 		<!-- Needed to keep the datepicker on top of the modal (popup) -->
 		.ui-datepicker{ z-index:1151 !important; }
+		
+		pre {
+	    white-space: pre-wrap;       /* CSS 3 */
+	    white-space: -moz-pre-wrap;  /* Mozilla, since 1999 */
+	    white-space: -pre-wrap;      /* Opera 4-6 */
+	    white-space: -o-pre-wrap;    /* Opera 7 */
+	    word-wrap: break-word;       /* Internet Explorer 5.5+ */
+		}
 	</style>
 </head>
 <body class="horizontal-menu">
